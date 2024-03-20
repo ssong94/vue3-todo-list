e01c650da7de2fb8f6daaecf859f9638743b950d
ssong94 <kyugwang@gmail.com>
2024-03-20 19:57:40 +0900

    feat: todo 진행상황, 검색 기능

--- src/components/views/TodoList.vue
+++ src/components/views/TodoList.vue
@@ -1,35 +1,66 @@
 <template>
-  <div style="align-content: center">
+  <div class="space-align-container">
+
   <div class="center">Todo List</div>
   <br/>
+
   <a-space direction="vertical">
     <a-input-search
         v-model:value="searchInput"
         placeholder="검색"
         enter-button
-        @input="filterTodos"
+        @keyup="changeValue"
     />
 
     <a-divider/>
+
+    <a-row :gutter="16">
+      <a-col :span="12">
+        <a-statistic title="진행상황" :value="checkedCount" class="demo-class">
+          <template #suffix>
+            <span>
+              /
+              {{ todos.length }}
+            </span>
+          </template>
+        </a-statistic>
+      </a-col>
+    </a-row>
 
     <a-input-group compact>
       <a-input-search
           v-model:value="todoInput"
           placeholder="할일"
           size="large"
-          show-count :maxlength="20"
+          show-count :maxlength="15"
           @keyup.enter="addNewTodo"
       >
         <template #enterButton>
           <a-button type="primary" @click="addNewTodo">➕</a-button>
         </template>
+
       </a-input-search>
+
     </a-input-group>
 
 
-    <a-checkbox v-for="todo in todos" :key="todo.id" v-model:checked="todo.checked ">{{ todo.text }}
-    <a-button type="primary" danger @click="deleteTodo(todo.id)">➖</a-button>
+    <a-checkbox v-for="todo in filterTodos" :key="todo.id" v-model:checked="todo.checked ">{{ todo.text }}
+
+      <a-popconfirm
+          title="삭제하시겠어요?"
+          ok-text="Yes"
+          cancel-text="No"
+          @confirm="deleteTodo(todo.id)"
+      >
+        <a-button type="text" danger>🗑️</a-button>
+      </a-popconfirm>
+
+
+
     </a-checkbox>
+
+    <a-empty v-if="todos.length === 0" />
+
 
   </a-space>
 
@@ -43,31 +74,41 @@
 
 
 import {computed, ref} from "vue";
+import { message } from 'ant-design-vue'
 
 const todos = ref([
   {
     id: 1,
-    text: 'test-todo1',
+    text: 'test-todo1 할일1 와',
     checked: false
   },
   {
     id:2,
-    text: 'test-todo2',
+    text: 'test-todo2 할일2',
     checked: true
   },
   {
     id:3,
-    text: 'test-todo3',
+    text: 'test-todo3 할일3',
     checked: false
   }
 ]
 )
 
+
 const searchInput = ref('');
 const todoInput = ref('');
 
+
+const checkedCount = computed(() => {
+  let count = 0;
+  todos.value.forEach(obj => obj.checked && count++)
+  return count;
+})
+
+
 const filterTodos = computed(() => {
-  console.log("dd")
+
   const searchText = searchInput.value.trim();
 
   if (!searchText) {
@@ -77,6 +118,16 @@ const filterTodos = computed(() => {
   return todos.value.filter(x => x.text.toLowerCase().includes(searchText.toLowerCase()))
 
 });
+
+const confirm = () => {
+  message.success('삭제되었어요.');
+};
+
+
+const changeValue = (e) => {
+  searchInput.value = e.target.value;
+}
+
 
 const addNewTodo = () => {
 
@@ -106,8 +157,8 @@ const createTodoObj = text => {
 const hasTextInTodos = value => todos.value.some(el => el.text === value);
 
 const deleteTodo = (id) => {
-  console.log("Delete todo with id: " + id);
   todos.value = todos.value.filter(obj => obj.id !== id);
+  confirm();
 }
 
 
