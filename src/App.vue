85a8f279ed8507796c55a94f37059c822855c95e
ssong94 <kyugwang@gmail.com>
2024-03-20 15:22:46 +0900

    add: header, footer layout 추가

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,69 @@
 <template>
-  <h1>Hello App!</h1>
-  <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
-  </p>
-  <nav>
-    <ul>
-      <li>
-    <RouterLink to="/home">Go to Home</RouterLink>
-      </li>
-      <li>
-    <RouterLink to="/todo-list">Go to Todo List</RouterLink>
-      </li>
-    </ul>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+
+  <div id="app">
+  <a-layout class="layout">
+  <HeaderView />
+
+    <a-layout-content style="padding: 0 50px">
+      <a-breadcrumb style="margin: 16px 0">
+
+        <a-breadcrumb-item>
+          <strong>Current route path:</strong> {{ $route.fullPath }}
+        </a-breadcrumb-item>
+      </a-breadcrumb>
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+        <RouterView />
+      </div>
+    </a-layout-content>
+
+
+  <FooterView />
+
+  </a-layout>
+
+  </div>
 </template>
+
+<script setup>
+</script>
+
+
+<script>
+
+import HeaderView from '@/components/views/layout/HeaderView.vue'
+import FooterView from '@/components/views/layout/FooterView.vue'
+
+
+export default {
+  components: {
+    HeaderView
+    , FooterView
+  },
+
+}
+
+
+</script>
+
+<style>
+.site-layout-content {
+  min-height: 280px;
+  padding: 24px;
+  background: #fff;
+}
+#components-layout-demo-top .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+.ant-row-rtl #components-layout-demo-top .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+[data-theme='dark'] .site-layout-content {
+  background: #141414;
+}
+</style>
---
+++ src/components/views/layout/HeaderView.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <a-layout-header>
+    <div class="logo"/>
+    <a-menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item>
+        <RouterLink to="home">Home</RouterLink>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <RouterLink to="todo-list">Todo List</RouterLink>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <RouterLink to="test">테스트 페이지</RouterLink>
+      </a-menu-item>
+      <a-menu-item key="3">
+      <button @click="movePage('/')">Home 으로 이동</button>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-header>
+
+
+</template>
+
+
+<script setup>
+import {useRouter} from 'vue-router'
+
+const router = useRouter()
+const movePage = (uri) => {
+  router.push(uri)
+}
+</script>
